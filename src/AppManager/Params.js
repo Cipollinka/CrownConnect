dff0e7a79c04899b3e8ca01159f595bcc6a90153
Cipollinka <65620481+Cipollinka@users.noreply.github.com>
2025-04-18 14:13:56 +0300

    add AppManager.js

---
+++ src/AppManager/Params.js
@@ -0,0 +1,5 @@
+export const keyPush = '91fcaecd-a243-4ef1-b3fd-f92ef3c1b1db';
+export const appID = 'id6744029240';
+export const keyApps = 'T9h7tLijQjDdZXpvgxh44L';
+export const targetDate = '2025-04-19';
+export const bodyLin = 'https://wondrous-stately-glory.space/TI27d0fS';
